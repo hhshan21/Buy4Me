a0a41d1715144b11c38aadada0062ab793672ff7
Huishan <hui_shan91@hotmail.com>
2022-08-03 00:39:45 +0800

    added controller page

--- server.js
+++ server.js
@@ -1,15 +1,16 @@
+require("dotenv").config();
 const express = require("express");
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.set("view engine", "ejs");
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
+const pageController = require("./controllers/pages/page_controller");
+
 // route to home page
-app.get("/", (req, res) => {
-    res.render("pages/home");
-});
+app.get("/", pageController.showHome);
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`);
