dbefac2009b362e1c807079429ad5fdd23dfb0ca
Huishan <hui_shan91@hotmail.com>
2022-08-07 00:47:19 +0800

    updated redirect route in user controller

--- controllers/users/users_controller.js
+++ controllers/users/users_controller.js
@@ -37,7 +37,7 @@ const controller = {
             console.log(err);
             res.send("Failed to create user");
         }
-        res.send("You are signed up!");
+        res.redirect("/login");
     },
 
     showLoginForm: (req, res) => {
