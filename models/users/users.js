3bc01e83fb46c28c520e794fac2613a4b30bd4ac
Huishan <hui_shan91@hotmail.com>
2022-08-06 11:44:36 +0800

    created user Schema

--- models/users/users.js
+++ models/users/users.js
@@ -1,15 +1,12 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
-const itemsRequestedSchema = new Schema({
-    username: { type: String },
-    img: { type: String },
-    description: { type: String },
-    country: { type: String },
-    quantity: { type: Number },
-    price_offered: { type: Number },
+const userSchema = new Schema({
+    name: { type: String, required: true },
+    email: { type: String, required: true },
+    hash: { type: String, required: true },
 });
 
-const ItemsRequested = mongoose.model("Items Requested", itemsRequestedSchema);
+const User = mongoose.model("User", userSchema);
 
-module.exports = ItemsRequested;
+module.exports = User;
