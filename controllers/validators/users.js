a9f8b3343d5774105d19c0a080c9723a1aa363f7
Huishan <hui_shan91@hotmail.com>
2022-08-08 16:24:43 +0800

    updated limit to username char

--- controllers/validators/users.js
+++ controllers/validators/users.js
@@ -2,7 +2,7 @@ const Joi = require("joi");
 
 const validators = {
     signUpValidator: Joi.object({
-        username: Joi.string().min(4).required(),
+        username: Joi.string().min(4).max(13).required(),
         email: Joi.string().email().required(),
         password: Joi.string().min(4).required(),
         confirm_password: Joi.string().required().valid(Joi.ref("password")),
