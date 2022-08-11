4c954a06ea476208ce51f8d579f1ddf94d2721ab
Huishan <hui_shan91@hotmail.com>
2022-08-11 21:21:34 +0800

    oly authUser can contact

--- controllers/pages/page_controller.js
+++ controllers/pages/page_controller.js
@@ -4,6 +4,7 @@ const Request = require("../../models/requests/request");
 const controller = {
     showHome: async (req, res) => {
         const requests = await Request.find().populate("user", "username");
+
         // const userInfo = await Users.find().populate("email");
         // console.log("page_contr requests: ", requests);
         // console.log("page_contr userInfo: ", userInfo);
